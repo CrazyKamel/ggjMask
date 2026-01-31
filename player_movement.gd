f0fc5293aba6753e03a6e2dafdf2dcc5d5907d29
MrAntex <jul31.chignard@gmail.com>
2026-01-31 17:15:56 +0100

    movement en grid

--- player_movement.gd
+++ player_movement.gd
@@ -1,13 +1,34 @@
 extends CharacterBody2D
 
-@export var step_length = 400
+@export var step_length = 128
+@export var speed = 250
 
-func get_input():
-	var input_direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = input_direction * step_length
-	
-	
+var target = position
 
+func _process(delta):
+	if velocity == Vector2(0,0):
+		if Input.is_action_just_pressed("Right"):
+			target.x = position.x + step_length
+			#position = target
+		elif Input.is_action_just_pressed("Left"):
+			target.x = position.x - step_length
+			#position = target
+		elif Input.is_action_just_pressed("Up"):
+			target.y = position.y - step_length
+			#position = target
+		elif Input.is_action_just_pressed("Down"):
+			target.y = position.y + step_length
+			#position = target
+		
+		
 func _physics_process(delta):
-	get_input()
+	velocity = position.direction_to(target) * speed
+	if position.distance_to(target) > 10:
+		move_and_slide()
+	else:
+		position = target
+		velocity = Vector2(0,0)
+	
+#func move():
+	#position = target
 	move_and_slide()
