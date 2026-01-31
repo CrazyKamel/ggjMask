22684a214bef75212d954ad823c607f198680bd4
Etincielle <140830712+etincielle@users.noreply.github.com>
2026-01-31 21:15:34 +0100

    Collisions (#8)
    * clamp mouvements
    
    * Collision TileMapLayer
    
    ---------
    
    Co-authored-by: MrAntex <jul31.chignard@gmail.com>
    Co-authored-by: Etincielle <axelle.rieu1@gmail.com>

--- player_movement.gd
+++ player_movement.gd
@@ -2,6 +2,13 @@ extends CharacterBody2D
 
 @export var step_length = 128
 @export var mask_strengh = 0
+@export var minX = 672
+@export var maxX = 1248
+@export var minY = 224
+@export var maxY = 800
+
+@onready var tileMap = $"../TileMapLayer"
+
 @onready var cam := get_viewport().get_camera_2d()
 @onready var colorRect = $"../ColorRect"
 
@@ -10,9 +17,13 @@ extends CharacterBody2D
 var target = position
 
 func _physics_process(delta):
+	var tile_coords = tileMap.local_to_map(tileMap.to_local(global_position))
+	print(tile_coords)
 	velocity = position.direction_to(target) * speed
 	if position.distance_to(target) > 10:
-		move_and_slide()
+		var collide = move_and_slide()
+		if (collide) :
+			velocity = Vector2(0,0)
 	else:
 		position = target
 		velocity = Vector2(0,0)
@@ -39,4 +50,4 @@ func _process(delta):
 	var viewport_size = get_viewport_rect().size
 	var screen_uv = screen_pos / viewport_size
 	mat.set_shader_parameter("player_screen_pos", screen_uv)
-	print(screen_uv)
+	#print(screen_uv)
